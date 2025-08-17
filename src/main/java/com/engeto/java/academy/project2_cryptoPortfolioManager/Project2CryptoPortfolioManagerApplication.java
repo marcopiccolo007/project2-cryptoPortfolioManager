286ef0c59faa7da6c4da56ba309460c3783ec9f6
marekmaly <marcopiccolo007@gmail.com>
2025-08-17 09:02:39 +0200

    vytvoreni testovaciho projektu cislo 2 (crypto porftolio manager) v ramci ENGETO Java Avademy

---
+++ src/main/java/com/engeto/java/academy/project2_cryptoPortfolioManager/Project2CryptoPortfolioManagerApplication.java
@@ -0,0 +1,13 @@
+package com.engeto.java.academy.project2_cryptoPortfolioManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2CryptoPortfolioManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2CryptoPortfolioManagerApplication.class, args);
+	}
+
+}
